2d0437a4acba59f359958e7bbbb69a0c46c8308b
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2024-08-24 05:55:34 +0300

    fix urls

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['optim.tildacdn.com'],
+    domains: ['optim.tildacdn.com','thumb.tildacdn.com','static.tildacdn.com'],
   }
 };
 
